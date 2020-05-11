6fb974bf7974d8c5d407237b118a5b9666b46533
cheng.yin <cheng_yin@jiliguala.com>
2020-05-11 19:33:45 +0800

    cd git dmeo

--- main.go
+++ main.go
@@ -118,6 +118,7 @@ func archive(target string, version string) {
 
 	*/
 	checkCMD("git")
+	excute("cd /Users/yc/Develop/Golang/GoShell/archive/gitdemo", false)
 	archiveInfo.User = gitConfig("user.name")
 	archiveInfo.Email = gitConfig("user.email")
 	merge(target, version)
