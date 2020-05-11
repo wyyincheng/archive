fa4e941a72e67cbd2a0b68db3a333317c10715dd
cheng.yin <cheng_yin@jiliguala.com>
2020-05-11 13:40:09 +0800

    search branch

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
@@ -17,7 +19,7 @@ func main() {
 		Usage: "archive appstore latest version which has been published.",
 		Action: func(c *cli.Context) error {
 			fmt.Println("start archive")
-			archive(os.Args[0])
+			archive(os.Args[1])
 			return nil
 		},
 		Flags: []cli.Flag{
@@ -33,34 +35,30 @@ func main() {
 		},
 		Commands: []*cli.Command{
 			{
-				Name:    "lock",
-				Aliases: []string{"l"},
-				Usage:   "lock tag or branch",
+				Name:  "lock",
+				Usage: "lock tag or branch",
 				Action: func(c *cli.Context) error {
 					fmt.Println("lock tag or branch")
 					return nil
 				},
 			},
 			{
-				Name:    "clean",
-				Aliases: []string{"c"},
-				Usage:   "clean tags and branches after archive",
+				Name:  "clean",
+				Usage: "clean tags and branches after archive",
 				Action: func(c *cli.Context) error {
 					return nil
 				},
 			},
 			{
-				Name:    "abort",
-				Aliases: []string{"a"},
-				Usage:   "rollback archive which version you given",
+				Name:  "abort",
+				Usage: "rollback archive which version you given",
 				Action: func(c *cli.Context) error {
 					return nil
 				},
 			},
 			{
-				Name:    "test",
-				Aliases: []string{"t"},
-				Usage:   "test cmd",
+				Name:  "test",
+				Usage: "test cmd",
 				Action: func(c *cli.Context) error {
 					test()
 					return nil
@@ -104,12 +102,28 @@ func merge(target string, from string) {
 	4.记录并merge
 	5.同步
 	*/
-	// excute("git", "checkout", "-f")
-	excute("git", "checkout", from)
-	excute("git", "pull")
-	excute("git", "checkout", target)
-	excute("git", "pull")
-	excute("git", "merge", from)
+	branch, err := search(from)
+	if err == nil {
+		excute("git", "checkout", "-f")
+		excute("git", "checkout", "-B", branch, "remote/"+branch)
+		excute("git", "pull")
+		excute("git", "checkout", target)
+		excute("git", "pull")
+		excute("git", "merge", branch)
+	}
+}
+
+func search(branch string) (string, error) {
+	// result := excute("git", "branch", "-r", "|", "grep", branch)
+	branches := strings.Split(excute("git", "branch", "-r"), "\n")
+	for _, info := range branches {
+		result := strings.Replace(info, " ", "", -1)
+		if strings.HasSuffix(result, branch) {
+			return result, nil
+		}
+	}
+	//deal error
+	return "", errors.New("branch not found")
 }
 
 func abort() {
@@ -136,7 +150,7 @@ func checkCMD(cmd string) {
 }
 
 func excute(name string, arg ...string) string {
-	fmt.Printf("cmd run: '%s' '%s'", name, arg)
+	fmt.Printf("cmd run: '%s' '%s'\n", name, arg)
 	cmd := exec.Command(name, arg...)
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
